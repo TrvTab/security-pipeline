4d05cf0bdfe4b434a2ff7b7ff142f639c551fcb9
TrvTab <ttabs15@outlook.com>
2025-11-12 16:41:59 -0500

    add terraform encryption for validity

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "foo-bucket" {
-#same resource configuration as previous example, but acl set for public access.
-  
-  acl           = "public-read"
-}
-data "aws_caller_identity" "current" {}
-
-
 resource "aws_efs_file_system" "sharedstore" {
   creation_token = var.efs["creation_token"]
 
@@ -14,7 +6,7 @@ resource "aws_efs_file_system" "sharedstore" {
   }
 
   kms_key_id                      = var.efs["kms_key_id"]
-  encrypted                       = false # false will fail checkov, true will passx
+  encrypted                       = true # false will fail checkov, true will passx
   performance_mode                = var.efs["performance_mode"]
   provisioned_throughput_in_mibps = var.efs["provisioned_throughput_in_mibps"]
   throughput_mode                 = var.efs["throughput_mode"]
