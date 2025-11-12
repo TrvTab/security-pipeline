9c5dcc8d32057450e1dccc2362651f79a80c4ff9
TrvTab <ttabs15@outlook.com>
2025-11-12 11:17:34 -0500

    change to sqlite3 for bandit

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import psycopg2
+import sqlite3
 
 app = Flask(__name__)
 
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/user')
 def get_user():
     user_id = request.args.get('id')
-    conn = psycopg2.connect('users.db')
+    conn = sqlite3.connect('users.db')
     query = f"SELECT * FROM users WHERE id = {user_id}"
-    cursor = conn.execute(query)  # Semgrep will catch this
+    cursor = conn.execute(query)
     return str(cursor.fetchone())
 
 # #  XSSd
